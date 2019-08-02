1a608c29afee1383c866c628a61686b79e91471f
Level.Z <1171567018@qq.com>
2019-08-02 11:53:18 +0800

    完善数据交互层

--- pkg/dao/mongodb.go
+++ pkg/dao/mongodb.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
 type MongoDB struct {
@@ -10,7 +13,11 @@ type MongoDB struct {
 }
 
 func (database *MongoDB) InitMongoDB() {
-	client, err := NewClient(options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
+
+	//fmt.Println(mongo.NewClient)
+	//client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://foo:bar@localhost:27017"))
 	fmt.Println(client, err)
 	//database.client = mongo
 	//database.mongodb = mongo.
