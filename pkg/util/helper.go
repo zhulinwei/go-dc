c7fb37b6a306a8cec1b91dc6fa0657d82ef5c71e
Level.Z <1171567018@qq.com>
2019-09-24 10:07:32 +0800

    更新项目

--- pkg/util/helper.go
+++ pkg/util/helper.go
@@ -2,6 +2,10 @@ package util
 
 import (
 	"context"
+	"fmt"
+	"github.com/zhulinwei/gin-demo/pkg/model"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"time"
 )
 
@@ -9,11 +13,27 @@ type Helper struct{}
 
 var helper = new(Helper)
 
-func GetHelper () *Helper {
+func GetHelper() *Helper {
 	return helper
 }
 
-func (*Helper) GetContent() (ctx context.Context) {
+func (Helper) GetContent() (ctx context.Context) {
 	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
 	return ctx
+}
+
+func (Helper) ParseServerConfig(filePath string) (model.Config, error) {
+	var err error
+	var fileBytes []byte
+	var serverConfig model.Config
+	if fileBytes, err = ioutil.ReadFile(filePath); err != nil {
+		fmt.Println(err)
+		return serverConfig, err
+	}
+	fmt.Println(fileBytes)
+	if err = yaml.Unmarshal(fileBytes, &serverConfig); err != nil {
+		return serverConfig, err
+	}
+	fmt.Println(serverConfig)
+	return serverConfig, nil
 }
