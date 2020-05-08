62489f68f239d2cf322dac7ad70d4ab198d7791f
Level.Z <1171567018@qq.com>
2020-05-08 12:28:11 +0800

    format

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,10 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/zhulinwei/go-dc/pkg/rpc"
-
-	"github.com/zhulinwei/go-dc/pkg/config"
-
 	"github.com/gin-gonic/gin"
+	"github.com/zhulinwei/go-dc/pkg/config"
 	"github.com/zhulinwei/go-dc/pkg/router"
+	"github.com/zhulinwei/go-dc/pkg/rpc"
 	"github.com/zhulinwei/go-dc/pkg/util/log"
 )
 
